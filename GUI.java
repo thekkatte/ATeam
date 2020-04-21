1f830f2f6b0b1ab9894824d2c0e76b15a18a65ca
thekkatte <61568073+thekkatte@users.noreply.github.com>
2020-04-21 09:21:44 -0500

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
   
-  //Temorary Lst of data
+  //Temporary List of data
   private List<String> list;
   
   private List<String> args;
@@ -27,48 +27,57 @@ public class GUI extends Application {
     list.add("2012-2");
     list.add("2019-3");
     
-    //creating the border pane
+    //creating the border pane for main page
     BorderPane bp1 = new BorderPane();
     
+    //buttons on the main page
     Button add = new Button("Add Data");
     Button read = new Button("Read Data");
     Button generate = new Button("Generate Report");
 
+    
+    //setting the text at the top of the layout
     bp1.setTop(new Label("What would you like to do?"));
     
+    
+    //HBox to equally space the buttons from each other
     HBox hbox = new HBox(250);
     hbox.getChildren().addAll(add,read,generate);
     
-    
-
+    //setting the center of the layout to the hbox
     bp1.setCenter(hbox);
 
-    
+    //setting the main scene - scene for the main page
     Scene mainScene = new Scene(bp1, 800, 800);
     
+    //App title is set to Milk Weights
     primaryStage.setTitle("Milk Weights");
     primaryStage.setScene(mainScene);
     primaryStage.show();  
     
     
-    
-    
-    
-    //Scene Read
+    //Scene for when the Read button is clicked
+   
+    //Border Pane for the page that opens when the read button is clicked
     BorderPane bp2 = new BorderPane();
     
+    //VBox with the data of file name of each year
+    // this needs to be changed as its supposed to be able to show the data after one of these file names are clicked
+    // hardcoded data is being used right now
     VBox vbox = new VBox();
     for(int i = 0 ; i < list.size(); i++) {
       vbox.getChildren().add(new Label(list.get(i)));
     }
     
-    //Button to return
-    Button back = new Button("back");
+    //Button to return from the read data page
+    Button back = new Button("Back");
     
+    //setting the second bp
     bp2.setTop(new Label("Read Data"));
     bp2.setCenter(vbox);
     bp2.setBottom(back);
     
+    //setting the read scene to bp2
     Scene readScene = new Scene(bp2, 800, 800);
     
     //setting action for read button
